881a23726743979174829e985d4a2a0a7ddae450
Chaz Jarman <chazjarman03@gmail.com>
2024-09-09 18:11:23 -0700

    Ternary Errors

--- SourceGeneration/TernaryInq.cs
+++ SourceGeneration/TernaryInq.cs
@@ -44,14 +44,30 @@ namespace SourceGeneration
                 var x = conditionalExpressionSyntax.GetText();
                 bool lineViolation = x.Lines.Count > 2;
                 bool nestedViolation = conditionalExpressionSyntax.DescendantNodes().Where(i => i as ConditionalExpressionSyntax is ConditionalExpressionSyntax).Count() >= 3;
+                if(lineViolation)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(TernaryDiagnosticDescriptors.TooManyLinesTernaryViolation, 
+                                            conditionalExpressionSyntax.GetLocation(), TernaryDiagnosticDescriptors.TMLVDescription));
+                }
                 if (nestedViolation)
                 {
-                    Console.WriteLine("LOCATED");
-                    context.AddSource(counter.ToString() + " Vioalation".AddGeneratorSuffix(), $"/////Ternary Violation: {conditionalExpressionSyntax.GetLocation().GetLineSpan().ToString()}");
+                    context.ReportDiagnostic(Diagnostic.Create(TernaryDiagnosticDescriptors.NestedTernaryViolation,
+                                            conditionalExpressionSyntax.GetLocation(), TernaryDiagnosticDescriptors.NTVDescription));
                 }
                 counter++;
             }
 
         }
     }
+    public static class TernaryDiagnosticDescriptors
+    {
+        public static DiagnosticDescriptor NestedTernaryViolation = new("CI_Ternary", "Nested Ternary Error", "'{0}'", "", DiagnosticSeverity.Error, true);
+
+        public static string NTVDescription = "Too many nested ternary operators, consider creating an if/else or swtich statement";
+
+        public static DiagnosticDescriptor TooManyLinesTernaryViolation = new("CI_Ternary", "Too Many Lines Error", "'{0}'", "", DiagnosticSeverity.Error, true);
+
+        public static string TMLVDescription = "Too many lines in ternary operation, consider creating an if/else or switch statement";
+    }
+
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,12 +2,7 @@
 MyClass myClass = new() { Id = 1 };
 Console.WriteLine(myClass.Id);
 MyStruct myStruct = new() { Id = 2 };
-bool x = (10 == myStruct.Id) ?
-    true : 
 
-
-    false;
-bool z = (true) ? x : false ? false : false ? x : false ? true : false;
 Console.WriteLine(myStruct.Id);
 class MyClass
 {
